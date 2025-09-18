a7b53c01a25c08ff1cb42435723a3acf5b99e735
siddesh-privy <siddesh@privy.io>
2025-09-18 18:39:29 +0700

    feat: smart wallets support

---
+++ examples/privy-next-smart-wallets/src/providers/providers.tsx
@@ -0,0 +1,29 @@
+// FILE ALTERED FROM CANONICAL STARTER
+"use client";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+import { SmartWalletsProvider } from "@privy-io/react-auth/smart-wallets";
+import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      // clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}
+      config={{
+        embeddedWallets: {
+          ethereum: {
+            createOnLogin: "users-without-wallets",
+          },
+          solana: {
+            createOnLogin: "users-without-wallets",
+          },
+        },
+        appearance: { walletChainType: "ethereum-and-solana" },
+        externalWallets: { solana: { connectors: toSolanaWalletConnectors() } },
+      }}
+    >
+      <SmartWalletsProvider>{children}</SmartWalletsProvider>
+    </PrivyProvider>
+  );
+}
