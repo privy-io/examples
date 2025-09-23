eeb4ff47b6ac8a88f163f15023ffde16838c46f9
siddesh-privy <siddesh@privy.io>
2025-09-24 00:10:47 +0700

    whitelabel: add missing deps and solana wallet action issue

--- privy-react-whitelabel-starter/app/components/SolanaWallet.tsx
+++ privy-react-whitelabel-starter/app/components/SolanaWallet.tsx
@@ -45,7 +45,11 @@ const SolanaWallet: React.FC<SolanaWalletProps> = ({wallet, index}) => {
 
   const customSignMessage = async () => {
     try {
-      const signature = await signMessage({message: Buffer.from('Your message here')});
+      const signature = await signMessage({
+        message: Buffer.from('Your message here'),
+        options: {address: wallet.address},
+      });
+
       toast.success(`Message signed successfully! ${signature}`);
     } catch (error: any) {
       toast.error(`Failed to sign message: ${error?.message}`);
