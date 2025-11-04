52eeef0ff6ac07bbf3eb14298a67550f32c67915
siddesh-privy <siddesh@privy.io>
2025-11-04 12:47:52 +0530

    style fix

--- examples/privy-next-funding/src/app/page.tsx
+++ examples/privy-next-funding/src/app/page.tsx
@@ -24,11 +24,11 @@ function Home() {
   }
 
   return (
-    <div className="bg-[#E0E7FF66] md:max-h-[100vh] md:overflow-hidden">
+    <div className="bg-[#E0E7FF66] md:max-h-screen md:overflow-hidden">
       <Header />
       {authenticated ? (
         <section className="w-full flex flex-col md:flex-row md:h-[calc(100vh-60px)]">
-          <div className="flex-grow overflow-y-auto h-full p-4 pl-8">
+          <div className="grow md:overflow-y-auto md:h-full p-4 pl-8 pb-4">
             <button className="button" onClick={logout}>
               <ArrowLeftIcon className="h-4 w-4" strokeWidth={2} /> Logout
             </button>
@@ -44,7 +44,9 @@ function Home() {
               <MFA />*/}
             </div>
           </div>
-          <UserObject />
+          <div className="md:shrink-0 md:overflow-y-auto md:h-full">
+            <UserObject />
+          </div>
         </section>
       ) : (
         <section className="w-full flex flex-row justify-center items-center h-[calc(100vh-60px)] relative">
@@ -57,7 +59,7 @@ function Home() {
           />
           <div className="z-10 flex flex-col items-center justify-center w-full h-full">
             <div className="flex h-10 items-center justify-center rounded-[20px] border border-white px-6 text-lg text-white font-abc-favorit">
-              Funding Demo
+                  Funding Demo
             </div>
             <div className="text-center mt-4 text-white text-7xl font-medium font-abc-favorit leading-[81.60px]">
               Starter repo
